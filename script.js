12160c8bf4d7593a550a52436ca32e2a317a831d
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-02-01 23:13:52 +0800

    Implemented a class for the explosion effect.

--- script.js
+++ script.js
@@ -79,6 +79,44 @@ class Raven {
   }
 }
 
+let explosions = [];
+class Explosion {
+  constructor(x, y, size) {
+    this.image = new Image();
+    this.image.src = "images/boom.png";
+    this.spriteWidth = 200;
+    this.spriteHeight = 179;
+    this.size = size;
+    this.x = x;
+    this.y = y;
+    this.frame = 0;
+    this.sound = new Audio();
+    this.sound.src = "boom.wav";
+    this.timeSinceLastFrame = 0;
+    this.frameInterval = 200;
+  }
+  update(deltaTime) {
+    if (this.frame === 0) this.sound.play();
+    this.timeSinceLastFrame += deltaTime;
+    if (this.timeSinceLastFrame > this.frameInterval) {
+      this.frame++;
+    }
+  }
+  draw() {
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.size,
+      this.size
+    );
+  }
+}
+
 function drawScore() {
   ctx.fillStyle = "black";
   ctx.fillText("Score: " + score, 55, 80);
